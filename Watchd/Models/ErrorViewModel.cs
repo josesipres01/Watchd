968a470473e996d18b45d57feeb717520e52ba86
José Sipres <a222206941@unison.mx>
2026-09-15 17:37:45 -0700

    Agregar archivos de proyecto.

---
+++ Watchd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Watchd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
